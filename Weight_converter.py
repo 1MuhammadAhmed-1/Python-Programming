74dca8f23d2cccd968a94612a071cce7291f231e
1MuhammadAhmed-1 <muhammad.ahmad3104@gmail.com>
2025-06-25 00:32:33 +0500

    Delete Weight_converter.py

--- Weight_converter.py
+++
@@ -1,8 +0,0 @@
-weight = input("Enter your weight: ")
-choice = input("(L)bs or (K)gs: ")
-pounds = int(weight) / 0.45
-kilograms = int(weight) * 0.45
-if choice.upper() == 'L':
-    print(f'{kilograms} kgs')
-elif choice.upper() == 'K' :
-    print(f'{pounds} lbs')
