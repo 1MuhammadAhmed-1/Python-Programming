fc434b3dc07deecf7e506fe7410e19f0c2ef7017
1MuhammadAhmed-1 <muhammad.ahmad3104@gmail.com>
2025-06-26 23:33:58 +0500

    Update automation.py

--- Automation_Python/automation.py
+++ Automation_Python/automation.py
@@ -1,23 +1,26 @@
 import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
-wb = xl.load_workbook(r'Automation_Python\transactions.xlsx')
-sheet = wb['Sheet1']
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
 
-for row in range(2 , sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    corrected_price = cell.value * 0.9
-    corrected_price_cell = sheet.cell(row, 4)
-    corrected_price_cell.value = corrected_price
+    for row in range(2 , sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
 
-values = Reference(sheet,
-          min_row = 2,
-          max_row = sheet.max_row,
-          min_col = 4,
-          max_col = 4)
+    values = Reference(sheet,
+            min_row = 2,
+            max_row = sheet.max_row,
+            min_col = 4,
+            max_col = 4)
 
-chart = BarChart()
-chart.add_data(values)
-sheet.add_chart(chart, 'e2')
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
 
-wb.save(r'Automation_Python\transactions2.xlsx')
+    wb.save(filename)
+file_to_process = input("Enter your files name: ")
+process_workbook(file_to_process)
